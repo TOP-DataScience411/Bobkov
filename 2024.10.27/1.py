241f5341ce842b3d4c201707bf2b598980f7753f
Gennadiy <renardes@gmail.com>
2024-11-03 11:42:57 +0500

    2024.10.27: правки оформления

--- 2024.10.27/1.py
+++ 2024.10.27/1.py
@@ -2,40 +2,44 @@ first_name = ""
 last_name = ""
 birth_year = "" 
 
-def main():  
-  global first_name, last_name, birth_year
+def main():
+    global first_name, last_name, birth_year
+    
+    # Запрос данных у пользователя
+    first_name = input("Ведите своё имя: ")
+    last_name = input("Ведие своё фамилия: ")
+    birth_year = input("Введите свой год рождения:")
+    
+    # Вывод данных, полученных от пользователя
+    print("\nСобранные данные:")
+    print("\nИмя:" + first_name)
+    print("Фамилия:" + last_name)
+    print("Год рождения:" + birth_year)
+    
+    # Пауза(Для отладки)
+    input("\n Нажмите Enter, чтобы выйти... ")
 
-  # Запрос данных у пользователя
-  first_name = input("Ведите своё имя: ")
-  last_name = input("Ведие своё фамилия: ")
-  birth_year = input("Введите свой год рождения:")
-  
-  # Вывод данных, полученных от пользователя
-  print("\nСобранные данные:")
-  print("\nИмя:" + first_name)
-  print("Фамилия:" + last_name)
-  print("Год рождения:" + birth_year)
-  
-  # Пауза(Для отладки)
-  input("\n Нажмите Enter, чтобы выйти... ")
+
 if __name__ == "__main__":
     main()
-    
+
+
 # D:\Ilya\Working\Bobkov\2024.10.27>python -i 1.py
 # Ведите своё имя: Илья
 # Ведие своё фамилия: Бобков
 # Введите свой год рождения:1991
-
+# 
 # Собранные данные:
-
+# 
 # Имя:Илья
 # Фамилия:Бобков
 # Год рождения:1991
-
- # Нажмите Enter, чтобы выйти... pbd
+# 
+#  Нажмите Enter, чтобы выйти... pbd
 # >>> first_name
 # 'Илья'
 # >>> last_name
 # 'Бобков'
 # >>> birth_year
 # '1991'
+
--- 2024.10.27/2.py
+++ 2024.10.27/2.py
@@ -11,17 +11,19 @@ def main():
     print(output)
     
     return num,output, next_number, previous_number
+
     
 if __name__ == "__main__":
     num, output, next_number, previous_number = main()
 
-#=============================================
+
+#======================================
 # Это результат в интерактивном режиме
-#=============================================
+#======================================
 
 # D:\Ilya\Working\Bobkov\2024.10.27>python -i 2.py
 # Введите число: 30
-
+# 
 # Следущее за числом 30 число: 31
 # Для число 30 предыдущее число: 29
 # >>> num
@@ -31,36 +33,44 @@ if __name__ == "__main__":
 # >>> previous_number
 # 29
 # >>> print(output)
-
+# 
 # Следущее за числом 30 число: 31
 # Для число 30 предыдущее число: 29
 # >>>
+
 
 #=============================================
 # Я конечно, не имею большого опыта ООП, но когда я хотел сделать задание через перегрузку, оказывается в Python нужно создавать для этого классы, что как по мне неудобно. Этот вариант я сделал для себя, как метод практики с классами.
 #=============================================
 
 # class MyInteger:
-   # def __init__(self, value):
-     # self.value = value
-     
-   # def __add__(self, value):
-     # return MyInteger(self.value + 1)
-     
-   # def __sub__(self, value):
-     # return MyInteger(self.value - 1)
-     
-   # def __str__(self):
-     # return str(self.value)
-    
+#     def __init__(self, value):
+#         self.value = value
+#     
+#     def __add__(self, value):
+#         return MyInteger(self.value + 1)
+#     
+#     def __sub__(self, value):
+#         return MyInteger(self.value - 1)
+#     
+#     def __str__(self):
+#         return str(self.value)
+# 
+# 
 # def main():    
-    # num = int(input("Введите число: "))
-    # my_integer = MyInteger(num)
-    
-    # output=(
-           # f"\nСледущее за числом {my_integer.value} число: {my_integer + 0}\n"
-           # f"Для числа {my_integer.value} предыдущее число: {my_integer - 0}"
-           # )
-    # print(output)
+#     num = int(input("Введите число: "))
+#     my_integer = MyInteger(num)
+#     
+#     output = (
+#         f"\nСледущее за числом {my_integer.value} число: {my_integer + 0}\n"
+#         f"Для числа {my_integer.value} предыдущее число: {my_integer - 0}"
+#     )
+#     print(output)
+# 
+# 
 # if __name__ == "__main__":
-    # main()
+#     main()
+
+
+# КОММЕНТАРИЙ: PEP 8 — сборник рекомендаций по стилистическому оформлению Python кода — их стоит использовать для большего удобства чтения своего и чужого кода: https://peps.python.org/pep-0008/
+
